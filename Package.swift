9b557e40b3966accc5406f47b90d91f2c349832b
jeffaburt <jburt1992@gmail.com>
2020-04-07 17:58:38 -0400

    add Package.swift for SPM support

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "QuizTrain",
+    platforms: [.macOS(.v10_12),
+                .iOS(.v10),
+                .tvOS(.v10),
+                .watchOS(.v3)],
+    products: [.library(name: "QuizTrain",
+                        targets: ["QuizTrain"])],
+    targets: [.target(name: "QuizTrain",
+                      path: "QuizTrain"),
+              .testTarget(name: "QuizTrainTests",
+                          dependencies: ["QuizTrain"],
+                          path: "QuizTrainTests")],
+    swiftLanguageVersions: [.v5])
